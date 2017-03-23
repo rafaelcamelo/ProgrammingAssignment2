d23d966d75ae88db2a2c27569f40c6b9b633d5b2
rafaelcamelo <rafael.camelo@gmail.com>
2017-03-22 21:43:43 -0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a list of functions and objects in cache that will be called to solve the matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        setmat <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        getmat <- function() x 
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m 
+        list(setmat = setmat, getmat = getmat,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function calls objects and functions to solve the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$getmat()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
+
+## Testing functions
+
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+
+cacheSolve(makeCacheMatrix(m1))
+
